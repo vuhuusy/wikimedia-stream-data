6dbb4eeabee85f906e531a817b71107d8c5ee924
vuhuusy <vuhuusytft1@gmail.com>
2024-04-25 19:52:40 +0700

    Commit changes

--- python-code/producer.py
+++ python-code/producer.py
@@ -1,36 +1,45 @@
 import requests
 from confluent_kafka import Producer
-from confluent_kafka.serialization import StringSerializer
 
-def fetch_wikimedia_changes():
+def fetch_recent_changes():
     url = "https://stream.wikimedia.org/v2/stream/recentchange"
     response = requests.get(url, stream=True)
     for line in response.iter_lines():
         if line:
-            yield line.decode('utf-8')
+            yield line
 
-def kafka_producer():
-    producer_config = {
-        'bootstrap.servers': 'localhost:9092'
-    }
-    producer = Producer(producer_config)
+
+def kafka_producer(config):
+    producer = Producer(config)
     return producer
 
-def produce_to_kafka(topic, data):
-    try:
-        producer = kafka_producer()
-        producer.produce(topic, value=data.encode('utf-8'))
-        producer.flush()
-    except Exception as e:
-        print("Error producing to Kafka:", e)
+
+def produce_to_kafka(producer, topic, message):
+    producer.produce(topic, value=message)
+    producer.flush()
+
+
+def print_kafka_config(config):
+    print("Kafka Configuration:")
+    for key, value in config.items():
+        print(f"{key}: {value}")
+
 
 def main():
-    topic = "wikimedia.recentchange"
-    for change in fetch_wikimedia_changes():
-        try:
-            produce_to_kafka(topic, change)
-        except Exception as e:
-            print("Error processing change:", e)
+    kafka_config = {
+        "bootstrap.servers": "localhost:9092",  # Kafka broker address
+        # "client.id": "wikimedia-stream-producer",
+        # Additional Kafka configuration can be added here
+    }
+    kafka_topic = "wikimedia_recentchange"
+
+    producer = kafka_producer(kafka_config)
+
+    print_kafka_config(producer.config)  # Print Kafka configuration
+
+    for change in fetch_recent_changes():
+        produce_to_kafka(producer, kafka_topic, change)
+
 
 if __name__ == "__main__":
     main()
